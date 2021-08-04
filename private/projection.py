c8c2eddf91715175aff716d5bd33538f45766ab6
jchzener <35633462+jchzener@users.noreply.github.com>
2021-08-03 22:18:19 -0400

    Update projection.py

--- private/projection.py
+++ private/projection.py
@@ -34,7 +34,7 @@ unit_cost = math.ceil(unit_cost_cfa/cfa2usd)
 def totalRevenue(unitPrice, quantity):
 	return math.floor(unitPrice*quantity)
 
-total_revenue = totalRevenue(400000, 40) + totalRevenue(455000, 150)
+total_revenue = totalRevenue(415000, 40) + totalRevenue(455000, 150)
 
 # Profit
 def profit(total_revenue, total_cost):
